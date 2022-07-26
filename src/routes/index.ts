49d085d621d443b254b49d0733d6d7c363d4a636
David Biolchini de Mello <biolchinid@gmail.com>
2022-07-26 16:47:59 -0300

    Add endpoints (#2)
    * endpoints: add endpoints
    
    * endpoints: add axios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,6 @@ import { Express } from 'express';
 import { MongoClient } from 'mongodb';
 
 export const router = (app: Express, mongoDB: MongoClient ) => {
+    console.log('a a')
     filmRoutes(app, mongoDB);
 }
